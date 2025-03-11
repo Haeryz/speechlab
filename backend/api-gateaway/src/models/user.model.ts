c1273cce4504bf5f2e758a3845a3b1881ec30182
Haeryz <haeriz42069@gmail.com>
2025-03-11 09:10:54 +0700

    Add timestamps option to user schema

--- backend/api-gateaway/src/models/user.model.ts
+++ backend/api-gateaway/src/models/user.model.ts
@@ -22,6 +22,6 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'VoiceProfile'
     }]
-});
+}, {timestamps: true});
 
 export default model('User', userSchema);
